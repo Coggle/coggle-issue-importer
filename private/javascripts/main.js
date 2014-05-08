0631b214823809a4acf83a673a4e87cc70b75ab0
James Crosby <james@coggle.it>
2014-05-08 20:57:25 +0100

    handle custom user/repo results better, keep the box around for more things; add generated files to repo

--- private/javascripts/main.js
+++ private/javascripts/main.js
@@ -5,50 +5,79 @@ $(document).ready(function(){
 
   var spinner = '<div class="spinner"> <div class="bounce1"></div> <div class="bounce2"></div> <div class="bounce3"></div> </div>';
 
-  function ingestIssuesForRepo(full_repo_name, replace_element_with_result){
-    
-    function fail(reason){
-      console.log("fail:", reason);
-      $('#error').html(
-        $('<div>', {text:reason})
-      );
-      $(replace_element_with_result).find('.spinner').remove();
-      $(replace_element_with_result).effect('shake');
-    }
-
-    $.ajax({
+  function ingestIssuesFor(full_repo_name){
+    return $.ajax({
              type: "POST",
               url: "/ingest/issues",
              data: JSON.stringify({full_repo_name:full_repo_name}),
       contentType: "application/json; charset=utf-8",
          dataType: "json"
-    }).fail(function(jqXHR, textStatus, errorThrown){
+    });
+  }
+
+  function createdButton(url, name){
+    return $('<div>', {html:'<a href="'+url+'">Issues for '+name+'</a>'}).addClass('created');
+  }
+
+  function displayError(text){
+      $('#error').html(
+        $('<div>', {text:text})
+      );
+  }
+
+  function clearError(){
+      $('#error').html('');
+  }
+
+  $('.ingestitem').on('click', function(){
+    var $self = $(this);
+    var repo_name = $self.attr('fullname');
+    $self.append($(spinner));
+
+    function fail(details){
+      displayError(details);
+      $self.find('.spinner').remove();
+      $self.effect('shake');
+    }
+
+    ingestIssuesFor(
+      repo_name
+    ).fail(function(jqXHR, textStatus, errorThrown){
       fail(textStatus);
     }).done(function(data, textStatus, jqXHR){
       if(data.error)
         return fail(data.details);
-      console.log("success:", data, textStatus);
-      $(replace_element_with_result).replaceWith(
-        $('<div>', {html:'<a href="'+data.url+'">Coggle created</a>'}).addClass('created')
-      );
-      $('#error').html('');
+      $self.replaceWith(createdButton(data.url, repo_name));
+      clearError();
     });
-  }
-
-  $('.ingestitem').on('click', function(){
-    var full_repo_name = $(this).attr('fullname');
-    console.log('item', full_repo_name , 'clicked');
-    $(this).append($(spinner));
-    ingestIssuesForRepo(full_repo_name, this);
   });
   
 
   $('.ingestinput').on('keydown', function(e){
+    var $self = $(this);
+
+    function fail(details){
+      displayError(details);      
+      $self.parent().find('.spinner').remove();
+      $self.effect('shake');
+    }
+
     if(e.keyCode == 13){
       // if enter was pressed
       e.preventDefault();
-      $(this).parent().append($(spinner));
-      ingestIssuesForRepo($(this).val(), $(this).parent());
+      $self.parent().append($(spinner));
+      var repo_name = $self.val();
+      ingestIssuesFor(
+        repo_name
+      ).fail(function(jqXHR, textStatus, errorThrown){
+        fail(details);
+      }).done(function(data, textStatus, jqXHR){
+        if(data.error)
+          return fail(data.details);
+        createdButton(data.url, repo_name).insertBefore($self);
+        $self.parent().find('.spinner').remove();
+        clearError();
+      });
     }
   });
 });
